a21bb9400707660ec0f43c3145e2b8494c27fb14
poojitha-mantena <52426390+poojitha-mantena@users.noreply.github.com>
2021-08-26 13:55:00 +0530

    Create Custom Widgets

--- app/src/main/java/com/example/myshoppal/activities/SplashScreen.kt
+++ app/src/main/java/com/example/myshoppal/activities/SplashScreen.kt
@@ -1,4 +1,4 @@
-package com.example.myshoppal
+package com.example.myshoppal.activities
 
 import android.content.Intent
 import android.os.Build
@@ -7,6 +7,8 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.WindowInsets
 import android.view.WindowManager
+import com.example.myshoppal.R
+
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
