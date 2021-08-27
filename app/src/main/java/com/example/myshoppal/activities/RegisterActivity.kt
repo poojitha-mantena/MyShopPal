f6e9028548e4452d068aa3c23ffb7aae46eda80e
poojitha-mantena <52426390+poojitha-mantena@users.noreply.github.com>
2021-08-27 14:07:34 +0530

    Validate Registy Entries

--- app/src/main/java/com/example/myshoppal/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/myshoppal/activities/RegisterActivity.kt
@@ -4,13 +4,16 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowInsets
 import android.view.WindowManager
 import com.example.myshoppal.R
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.activity_login.et_email as et_email1
+import kotlinx.android.synthetic.main.activity_login.et_password as et_password1
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -18,16 +21,77 @@ class RegisterActivity : AppCompatActivity() {
         @Suppress("DEPRECATION")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.hide(WindowInsets.Type.statusBars())
-        }else{
+        } else {
             window.setFlags(
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
-        tv_login.setOnClickListener{
+        setUpActionBar()
+
+        tv_login.setOnClickListener {
             val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
             startActivity(intent)
         }
 
+        btn_register.setOnClickListener{
+            validateRegisterDetails()
+        }
     }
+
+    private fun setUpActionBar() {
+
+        setSupportActionBar(toolbar_register_activity)
+
+        val actionBar = supportActionBar
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_back_24)
+        }
+
+        toolbar_register_activity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    private fun validateRegisterDetails(): Boolean {
+        return when {
+            TextUtils.isEmpty(et_first_name.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar("Please enter first name.", true)
+                false
+            }
+            TextUtils.isEmpty(et_last_name.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar("Please enter last name.", true)
+                false
+            }
+            TextUtils.isEmpty(et_email.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar("Please enter an email id.", true)
+                false
+            }
+            TextUtils.isEmpty(et_password.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar("Please enter a password.", true)
+                false
+            }
+            TextUtils.isEmpty(et_confirm_password.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar("Please enter confirm password.", true)
+                false
+            }
+            et_password.text.toString()
+                .trim { it <= ' ' } != et_confirm_password.text.toString()
+                .trim { it <= ' ' } -> {
+                showErrorSnackBar("Password and confirm password does not match", true)
+                false
+            }
+            !cb_terms_and_condition.isChecked -> {
+                showErrorSnackBar("Please agree terms and conditions.", true)
+                false
+            }
+            else -> {
+                showErrorSnackBar("Registration successfull.", false)
+                true
+            }
+
+        }
+    }
+
 }
