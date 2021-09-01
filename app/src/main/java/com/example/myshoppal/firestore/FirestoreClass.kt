4e800b43f575f85f1532062415a314cad8e6ca61
poojitha-mantena <52426390+poojitha-mantena@users.noreply.github.com>
2021-09-01 14:11:30 +0530

    Setup Firebase Firestore

---
+++ app/src/main/java/com/example/myshoppal/firestore/FirestoreClass.kt
@@ -0,0 +1,6 @@
+package com.example.myshoppal.firestore
+import com.google.firebase.firestore.FirebaseFirestore
+class FirestoreClass {
+
+    private val mFirestore = FirebaseFirestore.getInstance()
+}
