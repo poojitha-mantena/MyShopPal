b2a2fdc501f312eed120a9e14b8b1169fc4c55cf
poojitha-mantena <52426390+poojitha-mantena@users.noreply.github.com>
2021-09-01 13:22:56 +0530

    Implemention for forgot password activity

--- app/src/main/java/com/example/myshoppal/activities/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/myshoppal/activities/ForgotPasswordActivity.kt
@@ -2,17 +2,53 @@ package com.example.myshoppal.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.myshoppal.R
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_forgot_password.*
 import kotlinx.android.synthetic.main.activity_register.*
 
-class ForgotPasswordActivity : AppCompatActivity() {
+class ForgotPasswordActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
 
         setUpActionBar()
+
+        btn_submit.setOnClickListener {
+
+            val email: String = et_email_forgot_pw.text.toString().trim { it <= ' ' }
+
+            if (email.isEmpty()) {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+            } else {
+
+                showProgressDialog(resources.getString(R.string.please_wait))
+
+                FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                    .addOnCompleteListener { task ->
+
+                        // Hide the progress dialog
+                        hideProgressDialog()
+
+                        if (task.isSuccessful) {
+                            // Show the toast message and finish the forgot password activity to go back to the login screen.
+                            Toast.makeText(
+                                this@ForgotPasswordActivity,
+                                resources.getString(R.string.email_sent_success),
+                                Toast.LENGTH_LONG
+                            ).show()
+
+                            finish()
+                        } else {
+                            showErrorSnackBar(task.exception!!.message.toString(), true)
+                        }
+                    }
+            }
+        }
+
     }
+
     private fun setUpActionBar() {
 
         setSupportActionBar(toolbar_forgot_password_activity)
