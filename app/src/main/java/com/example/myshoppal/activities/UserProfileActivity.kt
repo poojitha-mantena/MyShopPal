507a530e086cfec2812609c0043bd80f4f6e063f
poojitha-mantena <52426390+poojitha-mantena@users.noreply.github.com>
2021-09-02 23:12:49 +0530

    Set permissions to store profile image

--- app/src/main/java/com/example/myshoppal/activities/UserProfileActivity.kt
+++ app/src/main/java/com/example/myshoppal/activities/UserProfileActivity.kt
@@ -1,14 +1,20 @@
 package com.example.myshoppal.activities
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.SyncStateContract
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.myshoppal.R
 import com.example.myshoppal.activities.models.User
 import com.example.myshoppal.utils.Constants
 import kotlinx.android.synthetic.main.activity_user_profile.*
 
-class UserProfileActivity : AppCompatActivity() {
+class UserProfileActivity : BaseActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
@@ -26,5 +32,40 @@ class UserProfileActivity : AppCompatActivity() {
 
         et_email.isEnabled = false
         et_email.setText(userDetails.email)
+
+        iv_user_photo.setOnClickListener(this@UserProfileActivity)
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+
+                R.id.iv_user_photo -> {
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        showErrorSnackBar("You already have the storage permission.", false)
+                    } else {
+                        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), Constants.READ_STORAGE_PERMISSION_CODE)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                showErrorSnackBar("The storage permission is granted.", false)
+            } else {
+                Toast.makeText(this, resources.getString(R.string.read_storage_permission_denied),
+                    Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/myshoppal/utils/Constants.kt
+++ app/src/main/java/com/example/myshoppal/utils/Constants.kt
@@ -5,4 +5,5 @@ object Constants {
     const val MY_SHOP_PAL_PREFERENCES: String = "myShopPalPreferences"
     const val LOGGED_IN_USERNAME: String = "logged_in_user_name"
     const val EXTRA_USER_DETAILS: String = "extra_user_details"
+    const val READ_STORAGE_PERMISSION_CODE = 2
 }
